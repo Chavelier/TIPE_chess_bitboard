1de484bcf824bff4e9ca0ed0d86c3aefb550af80
Chavelier <58261721+Chavelier@users.noreply.github.com>
2022-05-23 11:51:07 +0200

    implementation alphabeta
    ça marche ma gueule

--- engine.py
+++ engine.py
@@ -8,8 +8,9 @@ Created on Mon Mar 28 11:39:03 2022
 
 ENGINE
 """
-MIN_INT = -10000000
-MAX_INT = 10000000
+INFINITY=32000
+
+from init import *
 
 class Engine:
     """ classe qui gère l'ordinateur """
@@ -21,62 +22,47 @@ class Engine:
     def bot_move(self,depth,board):
         self.nodes = 0 # compteur des coups
         self.best_move = 0 #on initialise le meilleur coup
-        score = self.negamax(-500000,500000,depth,board)
+        score = self.alphabeta(-500000,500000,depth,board)
         # score = self.minmax(depth,board)
         return (self.best_move,score)
 
-    def negamax(self,alpha,beta,depth,board):
+
+
+    def alphabeta(self,alpha,beta,depth,b):
         """ algorithme de recherche du meilleur coup """
 
         if depth == 0:
-            return board.evaluation()
-
-        self.nodes += 1
-        old_alpha = alpha
-
-        move_list = board.move_generation(board.side)
-        for mv in move_list:
-            if not board.make_move(mv): # le coup n'est pas legal
-                continue # on le passe donc
-            self.ply += 1
-            score = -self.negamax(-beta,-alpha,depth-1,board)
-            board.undo_move(True)
-            self.ply -=1
-
-
-            if score >= beta:
-                return beta
-
-            if score > alpha:
-                alpha = score
-
-                if self.ply == 0 and old_alpha != alpha:
-                    self.best_move = mv # on associe comme meilleur coup de départ celui qui donne le meilleur score
-
-        return alpha
-
-
-
-
-    def minmax(self,depth,b):
-        # algorithme minmax sans élagage
-
-        if depth == 0: # ou mat
             return b.evaluation(False)
 
-        score = MIN_INT
+        self.nodes += 1
+        best_sofar = 0
+        old_alpha = alpha
 
         move_list = b.move_generation(b.side)
         for mv in move_list:
             if not b.make_move(mv): # le coup n'est pas legal
                 continue # on le passe donc
             self.ply += 1
-            old_score = score
-            score = max(score,-self.minmax(depth-1,b))
+            score = -self.alphabeta(-beta,-alpha,depth-1,b)
+            self.ply -=1
             b.undo_move(True)
-            self.ply -= 1
 
-            if self.ply == 0 and old_score != score:
-                self.best_move = mv
+            if score >= beta:
+                return beta
 
-        return score
+            if score > alpha: #on a trouvé un meilleur coup
+                alpha = score
+
+                if self.ply == 0:
+                    coup = CASES[b.get_move_source(mv)] + CASES[b.get_move_target(mv)] + PIECE_LETTER[b.get_move_promotion(mv)].lower()
+                    print("meilleur coup actuellement : %s \n"%coup)
+                    # self.best_move = mv
+                    best_sofar = mv # on associe comme meilleur coup de départ celui qui donne le meilleur score
+        
+        if old_alpha != alpha: #on a trouvé un meilleur coup
+            # coup = CASES[b.get_move_source(best_sofar)] + CASES[b.get_move_target(best_sofar)] + PIECE_LETTER[b.get_move_promotion(best_sofar)].lower()
+            # print("alpha : %s"%alpha)
+            # print("beta : %s"%beta)
+            # print("le meilleur coup a été actualisé à %s \n"%coup)
+            self.best_move = best_sofar
+        return alpha
